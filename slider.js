5078b062ba7cc84b18f4dadeec5a7afa603e355c
avdivrellaku <avdivrellaku@gmail.com>
2021-12-26 15:38:04 +0100

    modifikim ne slider dhe responsivitet

--- slider.js
+++ slider.js
@@ -1,17 +1,17 @@
-var sliderArray = ['images/slider1.png', 'images/slider2.png', 'images/slider3.png', 'images/slider4.png'];
+var sliderArray = ['url(images/slider1.png)', 'url(images/slider2.png)', 'url(images/slider3.png)', 'url(images/slider4.png)'];
 
 var i = 0;
 
 function slideShow() {
-    document.getElementById('slider').src = sliderArray[i];
+    document.getElementById('slides-pics').style.backgroundImage = sliderArray[i];
     if (i < sliderArray.length - 1) {
         i++;
-
+        document.getElementById('slides-pics').style.transition = "0.5s ease-in-out";
     }
     else {
         i = 0;
     }
-    document.getElementById('slider').style.transition = "opacity 2s";
+   
 
     setTimeout('slideShow()', 2000);
 
